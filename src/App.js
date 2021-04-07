be554b02dbaf04bdc2d0e9bb7a0b63f61d885f3c
spidergod <spidergod23@gmail.com>
2021-04-07 15:31:55 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from "react";
+import "./styles.css";
+
+export default class App extends PureComponent {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </div>
+    );
+  }
+}
