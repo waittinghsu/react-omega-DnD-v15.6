f65ad13d10a8dffab07ad4c86aacfb3d2aa92aa5
spidergod <spidergod23@gmail.com>
2021-04-07 17:24:56 +0800

    version 1.0.0 add file css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { PureComponent } from "react";
-import "./styles.css";
+import "./style/index.scss";
 
 export default class App extends PureComponent {
   render() {
     return (
       <div className="App">
+        <div className="d-flex flex-column red--bg black--text font-weight-black pt-3 mt-3 font-large rounded-lg">
+          <span>151</span>
+          <span>151</span>
+          <span>151</span>
+        </div>
         <h1>Hello CodeSandbox</h1>
-        <h2>Start editing to see some magic happen!</h2>
+        <h2>Start editing to see so9me magic happen!</h2>
       </div>
     );
   }
